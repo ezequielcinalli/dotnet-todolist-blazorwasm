17dc15f50b99b61697fbc6b109df075e38c3a130
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-07-25 09:16:19 -0300

    Improved todo item

--- Todo.cs
+++ Todo.cs
@@ -5,6 +5,13 @@ public class Todo
         this.Title = Title;
     }
 
+    public Todo(int Id, string Title)
+    {
+        this.Id = Id;
+        this.Title = Title;
+    }
+
+    public int? Id { get; set; } = 1;
     public string? Title { get; set; }
     public bool IsDone { get; set; }
 }
