5e49a03c082843f440ead78d556929ab7ce6bead
Ezequiel Cinalli <ezequielcinalli96@gmail.com>
2022-07-26 11:32:46 -0300

    Improved todo api project with in-memory db

--- TodoApi/Controllers/WeatherForecastController.cs
+++ TodoApi/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
-namespace backend.Controllers;
+namespace TodoApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
--- TodoApi/models/WeatherForecast.cs
+++ TodoApi/models/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace backend;
+namespace TodoApi.Models;
 
 public class WeatherForecast
 {
